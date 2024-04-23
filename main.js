ebb0808534d868ccaaf9b7ffaa9a924f59bb4c18
MuhammadHussain5066 <160261423+MuhammadHussain5066@users.noreply.github.com>
2024-04-23 21:18:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let Balance = 5000;
+let pin = 6789;
+let pinanswer = await inquirer.prompt([
+    {
+        name: "pincode",
+        message: "Enter your pin",
+        type: "number"
+    }
+]);
+if (pinanswer.pincode === pin) {
+    console.log("You have entered the right pin");
+    let Atmoperation = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Kindly select the option",
+            type: "list",
+            choices: ["Balance Inquiry", "Amount Withdraw"]
+        }
+    ]);
+    if (Atmoperation.operation === "Amount Withdraw") {
+        let amountanswer = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number"
+            }
+        ]);
+        Balance -= amountanswer.amount;
+        console.log(`Your remaining balance is ${Balance}`);
+    }
+    else if (Atmoperation.operation === "Balance Inquiry") {
+        console.log(`Your remaining balance is ${Balance}`);
+    }
+}
+else {
+    console.log("You have entered the wrong pin");
+}
